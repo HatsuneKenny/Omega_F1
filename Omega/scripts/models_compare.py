ade044be930fee16d1d41e1967c7bf87cc6d4e1a
Hatsune Kenny <127184407+HatsuneKenny@users.noreply.github.com>
2025-04-06 16:19:15 +0200

    Update models_compare.py

--- Omega/scripts/models_compare.py
+++ Omega/scripts/models_compare.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 import joblib
 from sklearn.metrics import accuracy_score, classification_report
+import sys
+sys.path.insert(0, 'vendor')
 
 # Load data
 df = pd.read_csv("../f1_race_results.csv")
